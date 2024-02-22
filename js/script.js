65f24da9b2673a58368c6935223e78a40b2dea40
Olga Smirnova <almazkacode@gmail.com>
2024-02-22 13:58:30 +0600

    feat: add scrolling animation in section skills

--- js/script.js
+++ js/script.js
@@ -53,6 +53,29 @@ let calcScrollValue = () => {
 
 window.addEventListener('scroll', calcScrollValue);
 
+// scrolling animation - section skills Intersection Observer API
+
+const options = {
+  root: null,
+  rootMargin: '5px',
+  threshold: 0.5,
+};
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+     change.target.classList.add('active');
+     observer.unobserve(change.target);
+    }
+  });
+}
+
+const observer = new IntersectionObserver(onEntry, options);
+
+const elements = document.querySelectorAll('.anim');
+elements.forEach(target => observer.observe(target));
+
+
 // validation
 
 let selector = document.getElementById('phone');
